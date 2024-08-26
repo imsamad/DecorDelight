c6a5c551fb8a6f145b07faeb20dd08757a957de5
Abdus Samad <imsamad00@gmail.com>
2024-08-26 22:35:45 +0530

    added

--- apps/web/src/app/Footer.tsx
+++ apps/web/src/app/Footer.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white py-8">
-      <div className="container mx-auto text-center">
+    <footer className='bg-black text-white py-8'>
+      <div className='container mx-auto text-center'>
         <p>
-          &copy; {new Date().getFullYear()} Your Antiques Store. All rights
+          &copy; {new Date().getFullYear()} Your DecorDelight Store. All rights
           reserved.
         </p>
       </div>
--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -1,8 +1,8 @@
-import { LogInIcon, Search, ShoppingCart, UserCircleIcon } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { getServerSession } from "next-auth";
-import { authOption } from "@/lib/authOption";
+import { LogInIcon, Search, ShoppingCart, UserCircleIcon } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { getServerSession } from 'next-auth';
+import { authOption } from '@/lib/authOption';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,8 +10,8 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { LogoutDropdownMenuItem } from "./LogoutDropdownMenuItem";
+} from '@/components/ui/dropdown-menu';
+import { LogoutDropdownMenuItem } from './LogoutDropdownMenuItem';
 
 export const Navbar = async () => {
   const session = await getServerSession(authOption);
@@ -19,51 +19,51 @@ export const Navbar = async () => {
   const isLoggedIn = !!session?.user.id;
 
   return (
-    <nav className="bg-transparent  backdrop-blur-md fixed  top-0 left-0 right-0 z-50 px-6 py-4 border-0 border-b-slate-200">
-      <div className="container mx-auto flex justify-between items-center p-0">
-        <div className="text-2xl font-bold mix-blend-difference">
-          <Link href="/">Antique</Link>
+    <nav className='bg-transparent  backdrop-blur-md fixed  top-0 left-0 right-0 z-50 px-6 py-4 border-0 border-b-slate-200'>
+      <div className='container mx-auto flex justify-between items-center p-0'>
+        <div className='text-2xl font-bold mix-blend-difference'>
+          <Link href='/'>DecorDelight</Link>
         </div>
 
-        <div className="flex-1 max-w-lg hidden md:block">
+        <div className='flex-1 max-w-lg hidden md:block'>
           <SearchBar />
         </div>
 
-        <div className="flex items-center ">
+        <div className='flex items-center '>
           {isLoggedIn ? (
             <>
-              <Link href="/cart" className="">
-                <Button variant="ghost" size="sm">
-                  <ShoppingCart className="w-5 h-5 mr-2" />
+              <Link href='/cart' className=''>
+                <Button variant='ghost' size='sm'>
+                  <ShoppingCart className='w-5 h-5 mr-2' />
                   My Cart
                 </Button>
               </Link>
               <DropdownMenu>
-                <DropdownMenuTrigger className="rounded-full">
-                  <UserCircleIcon className="w-8 h-8" />
+                <DropdownMenuTrigger className='rounded-full'>
+                  <UserCircleIcon className='w-8 h-8' />
                 </DropdownMenuTrigger>
-                <DropdownMenuContent className=" -translate-x-5 ">
+                <DropdownMenuContent className=' -translate-x-5 '>
                   <DropdownMenuLabel>
-                    <Link className="flex items-center" href="/me">
-                      <UserCircleIcon className="w-5 h-5 mr-2" />
+                    <Link className='flex items-center' href='/me'>
+                      <UserCircleIcon className='w-5 h-5 mr-2' />
                       My Profile
                     </Link>
                   </DropdownMenuLabel>
                   <DropdownMenuSeparator />
                   <DropdownMenuItem>
-                    <Link className="flex items-center" href="/me/orders">
-                      {session.user.role == "ADMIN" ? "All" : "My"} Orders
+                    <Link className='flex items-center' href='/me/orders'>
+                      {session.user.role == 'ADMIN' ? 'All' : 'My'} Orders
                     </Link>
                   </DropdownMenuItem>
-                  {session.user.role == "ADMIN" ? (
+                  {session.user.role == 'ADMIN' ? (
                     <DropdownMenuItem>
-                      <Link className="flex items-center" href="/me/products">
+                      <Link className='flex items-center' href='/me/products'>
                         All Products
                       </Link>
                     </DropdownMenuItem>
                   ) : null}
                   <DropdownMenuItem>
-                    <Link className="flex items-center" href="/me">
+                    <Link className='flex items-center' href='/me'>
                       My Cart
                     </Link>
                   </DropdownMenuItem>
@@ -75,9 +75,9 @@ export const Navbar = async () => {
             </>
           ) : (
             <>
-              <Link href="/me">
-                <Button variant="default" size="sm">
-                  <LogInIcon className="w-4 h-4 mr-2" /> Login
+              <Link href='/me'>
+                <Button variant='default' size='sm'>
+                  <LogInIcon className='w-4 h-4 mr-2' /> Login
                 </Button>
               </Link>
             </>
@@ -93,13 +93,13 @@ export const Navbar = async () => {
 
 const SearchBar = () => {
   return (
-    <div className="relative">
+    <div className='relative'>
       <input
-        type="text"
-        placeholder="Search..."
-        className="w-full py-2 px-4 pl-10 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        type='text'
+        placeholder='Search...'
+        className='w-full py-2 px-4 pl-10 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500'
       />
-      <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-500" />
+      <Search className='absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-500' />
     </div>
   );
 };
